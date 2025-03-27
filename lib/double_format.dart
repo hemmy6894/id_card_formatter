e09012ef3ab36f27ba04e5442769796f148fbf7f
Hemmy6894 <hmanyinja@gmail.com>
2025-03-27 11:56:31 +0300

    Refactor MoneyFormatter to manually format currency without intl dependency

--- lib/double_format.dart
+++ lib/double_format.dart
@@ -1,16 +1,30 @@
-import 'package:intl/intl.dart';
-
 extension MoneyFormatter on double {
   String toFormat({
-    String locale = 'en_US',
     String symbol = 'TZS',
     int decimalDigits = 0,
   }) {
-    final formatCurrency = NumberFormat.currency(
-      locale: locale,
-      symbol: '$symbol ',
-      decimalDigits: decimalDigits,
-    );
-    return formatCurrency.format(this);
+    final numStr = toStringAsFixed(decimalDigits);
+    final parts = numStr.split('.');
+    final integerPart = parts[0];
+    final fractionalPart = parts.length > 1 ? parts[1] : '';
+
+    final buffer = StringBuffer();
+    int count = 0;
+
+    for (int i = integerPart.length - 1; i >= 0; i--) {
+      buffer.write(integerPart[i]);
+      count++;
+      if (count % 3 == 0 && i != 0) {
+        buffer.write(',');
+      }
+    }
+
+    final formattedInt = buffer.toString().split('').reversed.join();
+
+    if (decimalDigits > 0) {
+      return '$symbol $formattedInt.$fractionalPart';
+    } else {
+      return '$symbol $formattedInt';
+    }
   }
 }
